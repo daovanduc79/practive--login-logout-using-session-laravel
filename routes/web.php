788547b3610705486220c5c2dc468ddace8712f3
daovanduc <duc07092000@gmail.com>
2020-06-01 12:03:23 +0700

    show form login

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.home');
 });
+Route::get('login', 'LoginController@showLogin')->name('show.login');
